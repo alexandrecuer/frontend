48455cde4ef3664d93002dfcf7b4ac31e281c218
Alexandre CUER <alexandre.cuer@cerema.fr>
2021-08-11 15:26:44 +0200

    how to use menuv3

---
+++ route.php
@@ -0,0 +1,33 @@
+<?php
+
+class Route
+{
+    public $controller = '';
+    public $action = '';
+    public $subaction = '';
+    public $format = "html";
+
+    public function __construct($q)
+    {
+        $this->decode($q);
+    }
+
+    public function decode($q)
+    {
+        // filter out all except a-z and / .
+        $q = preg_replace('/[^.\/A-Za-z0-9]/', '', $q);
+
+        // Split by /
+        $args = preg_split('/[\/]/', $q);
+
+        // get format (part of last argument after . i.e view.json)
+        $lastarg = sizeof($args) - 1;
+        $lastarg_split = preg_split('/[.]/', $args[$lastarg]);
+        if (count($lastarg_split) > 1) { $this->format = $lastarg_split[1]; }
+        $args[$lastarg] = $lastarg_split[0];
+
+        if (count($args) > 0) { $this->controller = $args[0]; }
+        if (count($args) > 1) { $this->action = $args[1]; }
+        if (count($args) > 2) { $this->subaction = $args[2]; }
+    }
+}
