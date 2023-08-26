46afa489a59a4e7de5a5ec1b58249166f1b8aea4
Alexandre CUER <alexandre.cuer@cerema.fr>
2023-08-26 22:55:31 +0200

    Update menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -314,7 +314,7 @@ var menu = {
             };
 	    document.body.addEventListener("click", function(e) {
                 var target = e.target;
-		console.log(target.getAttribute("l1");
+		console.log(target.getAttribute("l1"));
 	    }
             menu.active_l1 = $(this).attr("l1");
             let item = menu.obj[menu.active_l1];
